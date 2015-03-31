0f4e4a35af1a15c3f239af60794bcc35703f4be5
u1tralord <jacobpsemail@yahoo.com>
2015-03-31 03:51:18 +0000

    Initial Commit and Push!

---
+++ GitCommitAndPush.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+read -p "Commit description: " desc 
+
+current_time=$(date "+%Y.%m.%d-%H:%M:%S")
+echo [$current_time] - $desc >> updateLog.log
+
+git add .  
+git commit -m "$desc" 
+git push origin master
+
+echo "Git repository updated @ $current_time"
